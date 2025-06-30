df091fe37d9152f6bbd7c59baab564fecabe6eb1
drmostafazade <dr.mostafazade@gmail.com>
2025-06-30 15:02:39 +0330

    اولین کامیت از o2o

---
+++ odoo/release.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+RELEASE_LEVELS = [ALPHA, BETA, RELEASE_CANDIDATE, FINAL] = ['alpha', 'beta', 'candidate', 'final']
+RELEASE_LEVELS_DISPLAY = {ALPHA: ALPHA,
+                          BETA: BETA,
+                          RELEASE_CANDIDATE: 'rc',
+                          FINAL: ''}
+
+# version_info format: (MAJOR, MINOR, MICRO, RELEASE_LEVEL, SERIAL)
+# inspired by Python's own sys.version_info, in order to be
+# properly comparable using normal operators, for example:
+#  (6,1,0,'beta',0) < (6,1,0,'candidate',1) < (6,1,0,'candidate',2)
+#  (6,1,0,'candidate',2) < (6,1,0,'final',0) < (6,1,2,'final',0)
+version_info = (18, 0, 0, FINAL, 0, '')
+version = '.'.join(str(s) for s in version_info[:2]) + RELEASE_LEVELS_DISPLAY[version_info[3]] + str(version_info[4] or '') + version_info[5]
+series = serie = major_version = '.'.join(str(s) for s in version_info[:2])
+
+product_name = 'Odoo'
+description = 'Odoo Server'
+long_desc = '''Odoo is a complete ERP and CRM. The main features are accounting (analytic
+and financial), stock management, sales and purchases management, tasks
+automation, marketing campaigns, help desk, POS, etc. Technical features include
+a distributed server, an object database, a dynamic GUI,
+customizable reports, and XML-RPC interfaces.
+'''
+classifiers = """Development Status :: 5 - Production/Stable
+License :: OSI Approved :: GNU Lesser General Public License v3
+
+Programming Language :: Python
+"""
+url = 'https://www.odoo.com'
+author = 'OpenERP S.A.'
+author_email = 'info@odoo.com'
+license = 'LGPL-3'
+
+nt_service_name = "odoo-server-" + series.replace('~','-')
+
+version += '+e-20250526'
+
+repos_heads = {'odoo': 'a43ef8e15f4e0ee28bdb1975a8a359d3373e8d2c', 'enterprise': '8106d4b096ed2d72952ba6ff71bb1b592490d7f1', 'design-themes': '71cb50a9565b441192eecd8897eb2e4cc30f9f44'}
